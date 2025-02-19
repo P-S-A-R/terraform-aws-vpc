7428292d8d7b838990801d8c540ebde6c3c2e172
Satya Avinash Reddy Pamireddy <reddyavinash61@gmail.com>
2025-02-19 17:56:40 +0100

    aws-vpc

--- variables.tf
+++ variables.tf
@@ -35,3 +35,50 @@ variable "public_subnet_cidrs" {
 variable "public_subnet_tags" {
   default = {}
 }
+
+variable "private_subnet_cidrs" {
+   type = list
+   validation {
+    condition     = length(var.private_subnet_cidrs)  == 2
+    error_message = "please provide 2 private subnet cidr"
+  }
+  
+}
+
+variable "private_subnet_tags" {
+  default = {}
+}
+
+
+variable "database_subnet_cidrs" {
+   type = list
+   validation {
+    condition     = length(var.database_subnet_cidrs)  == 2
+    error_message = "please provide 2 database subnet cidr"
+  }
+  
+}
+
+variable "database_subnet_tags" {
+  default = {}
+}
+
+variable "nat_gateway_tags" {
+   default = {}
+  
+}
+
+variable "public_route_table_tags" {
+   default = {}
+  
+}
+
+variable "private_route_table_tags" {
+   default = {}
+  
+}
+
+variable "database_route_table_tags" {
+   default = {}
+  
+}
